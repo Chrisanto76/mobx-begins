39723f804ced4a83073454ac77881f770c1a80b7
Chrisanto76 <antonypoure2@gmail.com>
2023-11-29 09:22:42 +0100

    Refactor table structure in Roster component

--- src/Roster.tsx
+++ src/Roster.tsx
@@ -7,34 +7,36 @@ function Roster() {
 	const { players } = useTeamStore();
 	return (
 		<table>
-			<tr>
-				<th>Name</th>
-				<th>Age</th>
-				<th>Teams</th>
-				<th>Trade form</th>
-				<th>Is it their birthday ?</th>
-			</tr>
-			{players.map((athlete) => {
-				return (
-					<tr key={athlete.name}>
-						<td>{athlete.name}</td>
-						<td>{athlete.age}</td>
-						<td>{athlete.teamHistory}</td>
-						<td>
-							<TradeForm athlete={athlete} />
-						</td>
-						<td>
-							<button
-								type="button"
-								style={{ width: '100%' }}
-								onClick={() => athlete.wishHappyBirthday()}
-							>
-								whish happy birthday
-							</button>
-						</td>
-					</tr>
-				);
-			})}
+			<tbody>
+				<tr>
+					<th>Name</th>
+					<th>Age</th>
+					<th>Teams</th>
+					<th>Trade form</th>
+					<th>Is it their birthday ?</th>
+				</tr>
+				{players.map((athlete) => {
+					return (
+						<tr key={athlete.name}>
+							<td>{athlete.name}</td>
+							<td>{athlete.age}</td>
+							<td>{athlete.teamHistory}</td>
+							<td>
+								<TradeForm athlete={athlete} />
+							</td>
+							<td>
+								<button
+									type="button"
+									style={{ width: '100%' }}
+									onClick={() => athlete.wishHappyBirthday()}
+								>
+									whish happy birthday
+								</button>
+							</td>
+						</tr>
+					);
+				})}
+			</tbody>
 		</table>
 	);
 }
